72b77b726b4c4de59b4b59e2be73eab85f304c61
DJQ <13211080@bjtu.edu.cn>
2020-02-24 16:46:20 +0800

    add feature show log

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	r.GET("/test", func(c *gin.Context) {
+		c.String(http.StatusOK, "Your goMonitor is running!")
+	})
+	r.GET("/showLog", func(c *gin.Context) {
+		if log, err := getLogfile("test.log"); err != nil {
+			c.String(http.StatusInternalServerError, err.Error())
+		} else {
+			c.String(http.StatusOK, log)
+		}
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
---
+++ parse.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+//打开日志文件并以string返回文件全部内容
+func getLogfile(filename string) (string, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("File reading error: ", err)
+		return "", err
+	}
+	return string(data), nil
+}
+
+//解析文本中所有from到to中间的字符串，并存入string数组
+func parseFromTo(src, from, to string) []string {
+	var res []string
+	srcLen := len(src)
+	destLen := len(from)
+	start := 0
+	for loc := 0; loc < srcLen;  {
+		if loc = strings.Index(src[start:], from); loc > -1 {
+			if endLoc := strings.Index(src[start + loc:], to); endLoc > -1 {
+				value := src[start + loc + destLen : start + loc + endLoc]
+				res = append(res, value)
+				start += loc + endLoc
+			}
+		} else {
+			break
+		}
+	}
+	return res
+}
