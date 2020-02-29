43726071d2a0dff9778c78737c1a59a6d9b2eef6
DJQ <13211080@bjtu.edu.cn>
2020-02-29 16:41:09 +0800

    update setIp.sh, support set port

--- setIp.sh
+++ setIp.sh
@@ -1,3 +1,3 @@
-sed -i "s/127.0.0.1/$1/g" js/home.js
-sed -i "s/127.0.0.1/$1/g" js/analyse.js
-sed -i "s/127.0.0.1/$1/g" js/monitor.js
+sed -i "s/127.0.0.1/$1/g" js/*.js
+sed -i "s/8080/$2/g" js/*.js
+sed -i "s/8080/$2/g" main.go
